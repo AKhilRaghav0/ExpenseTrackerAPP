a466ee0a673fd9e84c77bb1a04b0da5635ea65f4
Akhil Raghav <86706467+AKhilRaghav0@users.noreply.github.com>
2023-11-24 12:52:45 +0530

    Create TransactionListViewModel.swift

---
+++ ExpenseTracker/TransactionListViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  TransactionListViewModel.swift
+//  ExpenseTracker
+//
+//  Created by Akhil on 24/11/23.
+//
+
+import Foundation
+
+final class TransactionListViewModel: ObservableObject {
+    @Published var transaction : [Transaction] = []
+    
+    func getTransactions() {
+        guard let url = URL(string: "https://designcode.io/data/transactions.json") else {
+            print("Invalid URL")
+            return
+        }
+        URLSession.shared.dataTaskPublisher(for: url)
+            .tryMap{ (data, response) -> Data in
+                guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+                    dump(response)
+                    throw URLError(.badServerResponse)
+                    
+                }
+                return data
+            }
+        
+        
+    }
+}
